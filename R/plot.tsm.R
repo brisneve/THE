3be46f47d5799d91db64c86d0761cd95e586ee01
brisneve <bedullantes@up.edu.ph>
2019-01-25 19:59:20 +0000

    updated plot.tsm

--- R/plot.tsm.R
+++ R/plot.tsm.R
@@ -1,4 +1,4 @@
-plot.tsm <- function (x) {
+plot.tsm <- function (x, destfile = NULL, width = 12, height = 8) {
 
   df2 <- x
 
@@ -10,14 +10,14 @@ plot.tsm <- function (x) {
     geom_hline(yintercept = df2$min_climatology, lty = 2, color = "blue")+
     geom_hline(yintercept = df2$max_climatology, lty = 2, color = "red")+
     geom_hline(yintercept = df2$max_climatology + 1, lty = 1, color = "red")+
-    theme_linedraw()+
+    theme_light()+
     ylab(expression(SST~(degree~C)))+
     scale_y_continuous(sec.axis = sec_axis( ~.*1, name = expression(SST~(degree~C))))+
     geom_smooth(method = "lm")+
     theme(axis.title.x=element_blank(),
           axis.text.x=element_blank(),
           axis.ticks.x=element_blank())
-  #theme(text = element_text(size=20))
+
 
   df3 <- df2 %>%
     mutate(year = lubridate:: year(date)) %>%
@@ -45,17 +45,27 @@ plot.tsm <- function (x) {
     geom_hline(yintercept = 8, lty = 2, color = "gray")+
     geom_point(data = df5, aes(x=date,y=max_dhw,color = DHW_label), size = 4)+
     geom_text(data = df5, aes(x=date,y=max_dhw, label=year, color = DHW_label),hjust=0, vjust=0)+
-    theme_linedraw()+
+    theme_light()+
     theme(legend.position = "none",
           legend.background = element_rect(fill ="transparent"))+
     labs(color = "Legend")+
     scale_color_manual(values = c("blue", "red"))
-  #theme(text = element_text(size=20))
 
   g <- cowplot:: plot_grid(p1,p2,
                            labels=c('A.', 'B.'), nrow = 2)
-  #label_size = 20, nrow = 2)
 
+  print.plot <- function(destfile, width,height){
+    pdf(destfile, width, height)
+
+    g <- cowplot:: plot_grid(p1 + theme(text = element_text(size=20)),
+                             p2 + theme(text = element_text(size=20)),
+                             labels=c('A.', 'B.'),
+                             label_size = 20, nrow = 2)
+    plot(g)
+    graphics.off()
+  }
+
+  invisible(if(!is.null(destfile)){print.plot(destfile, width, height)})
 
   print(g)
 }
