ba7b4907ff7dcd07410bd1b3832e003a2ec28ee3
brisneve <bedullantes@up.edu.ph>
2019-02-08 15:17:25 +0000

    Update extract.timeseries.cordat.R

--- R/extract.timeseries.cordat.R
+++ R/extract.timeseries.cordat.R
@@ -1,4 +1,8 @@
 extract.timeseries.cordat <- function(nc, varname, coor, fun = mean, method = "bilinear", buffer = 5000){
+  if(!require(tidyverse)){install.packages("tidyverse"); library(tidyverse)}
+  if(!require(imputeTS)){install.packages("imputeTS"); library(imputeTS)}
+  if(!require(lubridate)){install.packages("lubridate"); library(lubridate)}
+  if(!require(raster)){install.packages("raster"); library(raster)}
   nc.brick <- brick(nc, varname = varname)
   x <- raster:: extract(nc.brick,coor, method = method, buffer = buffer, fun = fun)
   x <- dplyr:: add_rownames(as.data.frame(t(x)))
